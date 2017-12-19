996c3d552076d73369e5fda87e7417d6ddac27c8
JTong <eldertong@gmail.com>
2017-12-19 15:00:24 -0500

    added downvote buttong with adnian and joe

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -67,6 +67,15 @@ class TopicsController < ApplicationController
     redirect_to(topics_path)
   end
 
+  def downvote
+    @topic = Topic.find(params[:id])
+    if @topic.votes.first != nil
+      bill = @topic.votes.first
+      @topic.votes.destroy(bill)
+    end
+    redirect_to(topics_path)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
